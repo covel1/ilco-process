71f008f88b2348f96999db1d8455b7ad65cede87
covel1 <co.velicu@gmail.com>
2018-09-09 17:30:07 +0300

    feat: Improve accesibility

--- js/ilco.js
+++ js/ilco.js
@@ -18,7 +18,6 @@ $('#navbarResponsive').on('shown.bs.collapse', function(e) {
 });
 
 function initLoad(){
-    //b2top();
     smoothMove();
 }
 
@@ -37,62 +36,6 @@ function smoothMove(){
         $('html,body').animate({
             scrollTop: oh
             }, 800);
-		//navig(this);suspended until a solution for server-side redirect to home is found
         });	
     });	
 }
-
-/**
-* Feed browser's history as such to be used by 'back' and 'forward' buttons of the browser
-
-
-window.onpopstate = function(event) {
-    if(event.state!==null){
-    var el = event.state.i;
-    //console.log(el);
-    var hi = $(el).offset().top-72;
-    $('html,body').animate({
-      scrollTop: hi
-      }, 800);
-    }
-    else {
-        $('html,body').animate({
-              scrollTop: 0
-          }, 800);
-    }
-};
-
-function navig (p){
-    if (p!==undefined){
-        var a = p;
-        var b = a.hash;
-		var c = b.slice(1);
-        history.pushState({i:b},'',c);    
-    }       
-}
-*/
-          
-
-/* Register events listeners */
-//$('#mynav > ul > li > a').on('click',function(){var that=this; mycollapse(that);});//navigation
-//$('#my_form').on('submit',function(){submitForm();return false;});//submit contact form
-/*------*/
-/* BACK TO TOP 
-function b2top(){
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-    $('#back-to-top').on('click',function (event) {
-        event.preventDefault();
-        $('#back-to-top').css('display','none');
-        $('html,body').animate({
-            scrollTop: 0
-        }, 800);
-    });
-    $('#back-to-top').css('display','block');
-}
-*/
