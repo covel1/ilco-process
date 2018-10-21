89c0917709c3b23c9e62363af21eea291ddbd658
covel1 <32466752+covel1@users.noreply.github.com>
2018-10-21 15:54:30 +0300

    Adapt fetch command for base url

--- js/ilco.js
+++ js/ilco.js
@@ -66,7 +66,7 @@ var int = `<div class="qzcontainer border">
 		</div>`;
 		
 document.getElementById('cvmp').innerHTML = int;
-fetch('../config.json').then(res => {return res.json()}).then(jsn => {use(jsn)});
+fetch('https://covel1.github.io/ilco-process/config.json').then(res => {return res.json()}).then(jsn => {use(jsn)});
 
 var qrys = [],
 answrs = [],
